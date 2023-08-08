364f262869a6a107c175db6ee87bed66c695ae9c
prakashr97 <prakashramasamy97@gmail.com>
2023-08-08 23:00:59 +0530

    deselEngine class added

--- vehicle.java
+++ vehicle.java
@@ -1,7 +1,9 @@
 public class vehicle {
 	public static void main(String arg[]) {
 		petrolEngine pe=new petrolEngine();
+		deselEngine de=new deselEngine();
 		System.out.println("Hello");
 		pe.Start();
+		de.Start();
 	}
 }
