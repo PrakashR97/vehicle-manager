e3c1c994447295e046242fc3ef2288f5a624fc70
prakashr97 <prakashramasamy97@gmail.com>
2023-08-08 22:48:36 +0530

    addded petroEngine class

---
+++ petrolEngine.java
@@ -0,0 +1,8 @@
+class petrolEngine{
+	public static void main(String args[])
+	{
+		System.out.println("Hello");
+		pE.start();
+	}
+	
+}
--- vehicle.java
+++ vehicle.java
@@ -1,5 +1,6 @@
 public class vehicle {
 	public static void main(String arg[]) {
+		petrolEngine pe=new petrolEngine();
 		System.out.println("Hello");
 		pe.Start();
 	}
