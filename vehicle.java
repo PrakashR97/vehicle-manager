2c36039f4160e77f3a84b6bd26abb7938bff8535
prakashr97 <prakashramasamy97@gmail.com>
2023-08-10 10:30:41 +0530

    Electric engine start method has been called in vehicle class

--- vehicle.java
+++ vehicle.java
@@ -2,12 +2,14 @@ public class vehicle {
 	public static void main(String arg[]) {
 		petrolEngine pe=new petrolEngine();
 		deselEngine de=new deselEngine();
+		ElectricEngine ee=new ElectricEngine();
 		System.out.println("Hello");
 		pe.Start();
 		de.Start();
 		pe.Stop();
 		de.Stop();
 		de.test();
+		ee.start();
 	
 	}
 }
